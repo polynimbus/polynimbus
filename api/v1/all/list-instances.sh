df5ca1e89eaae1cfd66c3ad15331862b8923451d
Tomasz Klim <github@tomaszklim.pl>
2018-09-20 23:16:38 +0200

    instances list is now easier to parse

--- api/v1/all/list-instances.sh
+++ api/v1/all/list-instances.sh
@@ -3,7 +3,6 @@
 for vendor in `ls /opt/polynimbus/drivers`; do
 	ACCS=`/opt/polynimbus/api/v1/account/list.sh $vendor`
 	for account in $ACCS; do
-		echo "# $vendor $account"
-		/opt/polynimbus/api/v1/instance/list.sh $vendor $account
+		/opt/polynimbus/api/v1/instance/list.sh $vendor $account |sed -e "s/^/$vendor $account /"
 	done
 done
