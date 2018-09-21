e6211be7cc1a1005a625f6de809ada7ce548cd19
Tomasz Klim <github@tomaszklim.pl>
2018-09-21 11:18:55 +0200

    list-users script for aws (prototype)

---
+++ drivers/aws/list-users.php
@@ -0,0 +1,14 @@
+#!/usr/bin/php
+<?php
+require_once "/opt/polynimbus/drivers/aws/internal/include.php";
+
+if ($argc < 2)
+	die("usage: $argv[0] <cloud-account>\n");
+
+$data = aws_request($argv[1], "iam list-users");
+
+foreach ($data["Users"] as $user) {
+	$username = $user["UserName"];
+	$created = substr($user["CreateDate"], 0, 10);
+	echo "$username $created\n";
+}
