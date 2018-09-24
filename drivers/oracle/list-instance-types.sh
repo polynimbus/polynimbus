1358f48fc9161637e98428253809513c15cc991d
Tomasz Klim <github@tomaszklim.pl>
2018-09-24 11:44:20 +0200

    fix for duplicated instance types in oracle cloud

--- drivers/oracle/list-instance-types.sh
+++ drivers/oracle/list-instance-types.sh
@@ -8,4 +8,4 @@ if [ ! -s $file ] || [ `stat -c %Y $file` -le `date -d '-8 hours' +%s` ]; then
 	oci compute shape list --compartment-id $compartment --all >$file
 fi
 
-grep '"shape":' $file |awk '{ print $2 }' |sed s/\"//g |sort
+grep '"shape":' $file |awk '{ print $2 }' |sed s/\"//g |sort |uniq
