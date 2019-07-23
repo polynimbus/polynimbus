9d7cf22d11a890da063fb2ccb21df3e77db3980a
Tomasz Klim <github@tomaszklim.pl>
2019-07-23 10:29:12 +0200

    check for azure command line client first

--- drivers/azure/test-account.sh
+++ drivers/azure/test-account.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if [ "`which az 2>/dev/null`" = "" ]; then
+	echo "error: Azure command line client not found"
+	exit 0
+fi
+
 if [ -f /root/.azure/accessTokens.json ] && [ "`az account list-locations`" = "" ]; then
 	echo "error: Azure account not configured, or without active subscriptions"
 fi
