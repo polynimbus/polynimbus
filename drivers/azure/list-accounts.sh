2b333b6930f2c476cc1a8cd9c18313a49f069ee3
Tomasz Klim <github@tomaszklim.pl>
2019-07-23 12:44:27 +0200

    check for azure command line client first

--- drivers/azure/list-accounts.sh
+++ drivers/azure/list-accounts.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-if [ "`az account list |grep Enabled`" != "" ]; then
+if [ "`which az 2>/dev/null`" != "" ] && [ "`az account list |grep Enabled`" != "" ]; then
 	/opt/polynimbus/drivers/azure/list-available-regions.sh
 fi
