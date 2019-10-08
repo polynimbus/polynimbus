7216111e8f688355f3ffe4324d63c1916f39f14e
Tomasz Klim <github@tomaszklim.pl>
2019-10-08 22:28:24 +0200

    support both attached and inline aws user/group policies

--- drivers/aws/list-attached-policies.php
+++ drivers/aws/list-attached-policies.php
@@ -7,13 +7,28 @@ if ($argc < 4)
 
 $_name = escapeshellarg($argv[3]);
 
-if ($argv[2] == "user") {
+if ($argv[2] == "user")
+{
 	$data = aws_request($argv[1], "iam list-attached-user-policies --user-name $_name");
-} else if ($argv[2] == "group") {
+	foreach ($data["AttachedPolicies"] as $policy)
+		echo $policy["PolicyName"]."\n";
+
+	$data = aws_request($argv[1], "iam list-user-policies --user-name $_name");
+	foreach ($data["PolicyNames"] as $policyname)
+		echo "$policyname\n";
+}
+else if ($argv[2] == "group")
+{
 	$data = aws_request($argv[1], "iam list-attached-group-policies --group-name $_name");
-} else {
+	foreach ($data["AttachedPolicies"] as $policy)
+		echo $policy["PolicyName"]."\n";
+
+	$data = aws_request($argv[1], "iam list-group-policies --group-name $_name");
+	foreach ($data["PolicyNames"] as $policyname)
+		echo "$policyname\n";
+}
+else
+{
 	die("error: invalid mode, should either user or group\n");
 }
 
-foreach ($data["AttachedPolicies"] as $policy)
-	echo $policy["PolicyName"]."\n";
