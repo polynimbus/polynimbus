9fa792a300212ea76d1b12f5cc854f1a7e7ec448
Tomasz Klim <github@tomaszklim.pl>
2019-10-10 17:08:02 +0200

    fixed regions list for hetzner robot

--- drivers/robot/list-available-regions.sh
+++ drivers/robot/list-available-regions.sh
@@ -6,4 +6,4 @@ if [ ! -s $file ] || [ `stat -c %Y $file` -le `date -d yesterday +%s` ]; then
 	curl -s https://wiki.hetzner.de/index.php/Rechenzentren_und_Anbindung/en >$file
 fi
 
-egrep -o '<th colspan="2"> (.+)' $file |awk '{ print $3 }' |tr '[:upper:]' '[:lower:]'
+egrep -o '<th colspan="2"> (.+)' $file |awk '{ print $3 }' |cut -d'<' -f1 |tr '[:upper:]' '[:lower:]'
