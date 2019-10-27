d30caeef1f40994d741d85ffbb41413701e1878c
Tomasz Klim <github@tomaszklim.pl>
2019-10-27 16:53:08 +0100

    avoid using api where not necessary

--- inventory/helpers/list-encryption-keys.sh
+++ inventory/helpers/list-encryption-keys.sh
@@ -4,7 +4,7 @@
 
 accounts=`/opt/polynimbus/drivers/aws/list-accounts.sh`
 for account in $accounts; do
-	regions=`/opt/polynimbus/api/v1/region/list-available.sh aws $account`
+	regions=`/opt/polynimbus/drivers/aws/list-available-regions.sh $account`
 	for region in $regions; do
 		/opt/polynimbus/drivers/aws/list-encryption-keys.php $account $region |sed -e "s/^/aws $account /"
 	done
