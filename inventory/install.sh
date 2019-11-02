aa6a3e459384d9964aafd634cb00993095144029
Tomasz Klim <github@tomaszklim.pl>
2019-11-02 16:49:41 +0100

    create and use blacklist files for aws to avoid access denied errors

--- inventory/install.sh
+++ inventory/install.sh
@@ -10,10 +10,15 @@ chown root:www-data /var/cache/polynimbus
 chown www-data:www-data /var/cache/polynimbus/inventory
 
 touch \
-	/var/cache/polynimbus/aws/list-sg.blacklist \
+	/var/cache/polynimbus/aws/list-nosql.blacklist \
 	/var/cache/polynimbus/aws/list-users.blacklist \
 	/var/cache/polynimbus/aws/list-zones.blacklist \
+	/var/cache/polynimbus/aws/list-trails.blacklist \
+	/var/cache/polynimbus/aws/list-cognito.blacklist \
+	/var/cache/polynimbus/aws/list-compute.blacklist \
 	/var/cache/polynimbus/aws/list-storage.blacklist \
+	/var/cache/polynimbus/aws/list-databases.blacklist \
+	/var/cache/polynimbus/aws/list-encryption.blacklist \
 	/var/cache/polynimbus/aws/list-serverless.blacklist \
 	/var/cache/polynimbus/google/api.blacklist \
 	/var/cache/polynimbus/google/get-iam-policy.blacklist
@@ -23,6 +28,7 @@ if ! grep -q /opt/polynimbus/inventory/cron /etc/crontab; then
 	echo "$((RANDOM%60))  */6 * * * root /opt/polynimbus/inventory/cron/process-compute-instances.sh" >>/etc/crontab
 	echo "$((RANDOM%60))  */4 * * * root /opt/polynimbus/inventory/cron/process-object-storage.sh" >>/etc/crontab
 	echo "$((RANDOM%60)) 7,17 * * * root /opt/polynimbus/inventory/cron/process-dns.sh" >>/etc/crontab
+	echo "$((RANDOM%60)) 7,17 * * * root /opt/polynimbus/inventory/cron/process-pools.sh" >>/etc/crontab
 	echo "$((RANDOM%60)) 7,17 * * * root /opt/polynimbus/inventory/cron/process-databases.sh" >>/etc/crontab
 	echo "$((RANDOM%60)) 8,18 * * * root /opt/polynimbus/inventory/cron/process-serverless.sh" >>/etc/crontab
 	echo "$((RANDOM%60)) 8,18 * * * root /opt/polynimbus/inventory/cron/process-users.sh" >>/etc/crontab
