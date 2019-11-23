c8b1b1d22fdedbbd8210ff3a1ad33fa9fb6008f8
Tomasz Klim <github@tomaszklim.pl>
2019-11-24 00:35:19 +0100

    raw mode support in azure instance types list

--- drivers/azure/list-instance-types.sh
+++ drivers/azure/list-instance-types.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ "$1" = "" ]; then
-	echo "usage: $0 <cloud-account> [region]"
+	echo "usage: $0 <cloud-account> [region] [--raw]"
 	exit 1
 elif [ ! -f /etc/polynimbus/azure/$1.sh ]; then
 	echo "error: cloud account \"$1\" not configured"
@@ -22,4 +22,8 @@ if [ ! -s $file ] || [ `stat -c %Y $file` -le `date -d yesterday +%s` ]; then
 	az vm list-sizes --subscription $AZURE_SUBSCRIPTION --location $region >$file
 fi
 
-grep '"name"' $file |awk '{ print $2 }' |sed -e s/\"//g -e s/,//g |sort
+if [ "$3" = "--raw" ]; then
+	cat $file
+else
+	grep '"name"' $file |awk '{ print $2 }' |sed -e s/\"//g -e s/,//g |sort
+fi
