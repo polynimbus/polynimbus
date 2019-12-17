097d880181d3176585ff66dc6e95bdf7ab6f0725
Tomasz Klim <github@tomaszklim.pl>
2019-12-17 13:14:47 +0100

    mute profiles sync for polynimbus backup

--- inventory/cron/sync-backup-configuration.sh
+++ inventory/cron/sync-backup-configuration.sh
@@ -3,7 +3,7 @@
 key=$1
 target=root@$2
 
-rsync -e "ssh -i $key" -av --delete /var/cache/polynimbus/aws/s3cmd $target:/var/cache/polynimbus/aws
-rsync -e "ssh -i $key" -av --delete /var/cache/polynimbus/azure/storage-accounts $target:/var/cache/polynimbus/azure
+rsync -e "ssh -i $key" -a --delete /var/cache/polynimbus/aws/s3cmd $target:/var/cache/polynimbus/aws
+rsync -e "ssh -i $key" -a --delete /var/cache/polynimbus/azure/storage-accounts $target:/var/cache/polynimbus/azure
 scp -p -i $key /var/cache/polynimbus/inventory/object-storage.list $target:/var/cache/polynimbus/inventory
 scp -p -i $key /var/cache/polynimbus/aws/s3-backup.blacklist $target:/var/cache/polynimbus/aws
