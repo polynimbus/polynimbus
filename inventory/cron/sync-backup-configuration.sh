5a075250302d1480978cf3cb1a889240169aecb5
Tomasz Klim <github@tomaszklim.pl>
2020-01-12 16:14:54 +0100

    backup support for google cloud storage

--- inventory/cron/sync-backup-configuration.sh
+++ inventory/cron/sync-backup-configuration.sh
@@ -7,5 +7,7 @@ rsync -e "ssh -i $key" -a --delete /var/cache/polynimbus/aws/s3cmd $target:/var/
 rsync -e "ssh -i $key" -a --delete /var/cache/polynimbus/linode/s3cmd $target:/var/cache/polynimbus/linode
 rsync -e "ssh -i $key" -a --delete /var/cache/polynimbus/azure/storage-accounts $target:/var/cache/polynimbus/azure
 rsync -e "ssh -i $key" -a --delete /etc/polynimbus/b2 $target:/etc/polynimbus
+rsync -e "ssh -i $key" -a --delete /root/.gsutil $target:/root
+rsync -e "ssh -i $key" -a --delete /root/.config/gcloud $target:/root/.config
 scp -p -i $key /var/cache/polynimbus/inventory/storage.list $target:/var/cache/polynimbus/inventory
 scp -p -i $key /var/cache/polynimbus/aws/s3-backup.blacklist $target:/var/cache/polynimbus/aws
