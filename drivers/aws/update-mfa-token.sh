cba9c76a2c3090a822d4fb3e5aaee61ba008993e
Tomasz Klim <github@tomaszklim.pl>
2020-02-11 00:17:41 +0100

    aws update mfa token script fixed after tests

--- drivers/aws/update-mfa-token.sh
+++ drivers/aws/update-mfa-token.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-if [ "$4" = "" ]; then
-	echo "usage: $0 <cloud-account> <role-arn> <mfa-arn> <mfa-token>"
+if [ "$5" = "" ]; then
+	echo "usage: $0 <cloud-account> <new-profile> <role-arn> <mfa-arn> <mfa-token>"
 	exit 1
 elif ! grep -q "\[$1\]" /root/.aws/credentials; then
 	echo "error: cloud account \"$1\" not configured"
 	exit 1
-elif ! [[ $4 =~ ^[0-9]{6}$ ]]; then
-	echo "error: parameter $4 not conforming mfa token code format"
+elif ! [[ $5 =~ ^[0-9]{6}$ ]]; then
+	echo "error: parameter $5 not conforming mfa token code format"
 	exit 1
 fi
 
 account=$1
-role_arn=$2
-mfa_arn=$3
-token=$4
+profile=$2
+role_arn=$3
+mfa_arn=$4
+token=$5
 
 tmpfile=`mktemp`
 aws sts assume-role \
@@ -27,15 +28,17 @@ aws sts assume-role \
 	--output json >$tmpfile
 
 if [ $? -eq 0 ]; then
-	sed -i -e "/\[$account\]/,+3 d" /root/.aws/credentials
+	sed -i -e "/\[$profile\]/,+3 d" /root/.aws/credentials
 
 	AWS_SESSION_TOKEN=`cat $tmpfile |jq -r '.Credentials.SessionToken'`
 	AWS_ACCESS_KEY_ID=`cat $tmpfile |jq -r '.Credentials.AccessKeyId'`
 	AWS_SECRET_ACCESS_KEY=`cat $tmpfile |jq -r '.Credentials.SecretAccessKey'`
 
-	echo "[$account]
+	echo "[$profile]
 aws_session_token = ${AWS_SESSION_TOKEN}
 aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
 " >>/root/.aws/credentials
 fi
+
+rm -f $tmpfile
