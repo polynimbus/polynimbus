2cb5a226d22641e7df7613641eed374ea503d87d
Tomasz Klim <github@tomaszklim.pl>
2022-02-10 12:11:44 +0100

    gather azure per-region usage in proper way

--- inventory/helpers/azure/list-usage.sh
+++ inventory/helpers/azure/list-usage.sh
@@ -2,7 +2,7 @@
 
 account=$1
 
-regions=`/opt/polynimbus/api/v1/region/list-available.sh azure $account |egrep -v "(australiacentral2|francesouth|germany|norway|southafricawest|switzerland|uaecentral)"`
+regions=`/opt/polynimbus/api/v1/region/list-available.sh azure $account |grep -xFf /opt/polynimbus/drivers/azure/config/regions-supporting-usage.conf`
 for region in $regions; do
 	/opt/polynimbus/drivers/azure/list-usage.sh $account $region |sed -e "s/^/$region /"
 done
